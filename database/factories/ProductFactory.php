45a4a356d1c170dd3d63dba19f72302a1e825296
Dwi Cahyo Mulyo Nugroho <schatze@Dwis-MacBook-Pro.local>
2023-09-09 23:16:49 +0700

    Database Schema definition

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->unique()->name(),
+            'category_id' => Category::inRandomOrder()->first()->id,
+            'image' => fake()->imageUrl(360, 360, 'animals', true, 'dogs', true, 'jpg'),
+            'price' => fake()->numberBetween($min = 10, $max = 100)
+        ];
+    }
+}
